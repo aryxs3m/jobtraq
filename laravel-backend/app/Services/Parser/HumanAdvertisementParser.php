e5f786f4ae51b259d1de558d7967eba6a3fffeca
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2023-08-02 08:42:58 +0200

    fix HumanAdvertisementParser initialization

--- laravel-backend/app/Services/Parser/HumanAdvertisementParser.php
+++ laravel-backend/app/Services/Parser/HumanAdvertisementParser.php
@@ -18,42 +18,10 @@ class HumanAdvertisementParser
 
     protected static array $locations = [];
 
-    public function __construct()
-    {
-        if ([] === self::$levels) {
-            /** @var JobLevel $item */
-            foreach (JobLevel::all() as $item) {
-                self::$levels[$item->name] = $item->keywords;
-            }
-        }
-
-        if ([] === self::$stacks) {
-            /** @var JobStack $item */
-            foreach (JobStack::all() as $item) {
-                self::$stacks[$item->name] = [
-                    'keywords' => $item->keywords,
-                    'defaultPosition' => $item->jobPosition ? $item->jobPosition->name : null,
-                ];
-            }
-        }
-
-        if ([] === self::$positions) {
-            /** @var JobPosition $item */
-            foreach (JobPosition::all() as $item) {
-                self::$positions[$item->name] = $item->keywords;
-            }
-        }
-
-        if ([] === self::$locations) {
-            /** @var Location $item */
-            foreach (Location::all() as $item) {
-                self::$locations[$item->location] = $item->id;
-            }
-        }
-    }
-
     public function parseJobTitle(string $title): JobCategory
     {
+        $this->initParserData();
+
         $titleParts = explode(' ', str_replace(['(', ')', '/', ','], ' ', mb_strtolower($title)));
         $jobCategory = new JobCategory();
 
@@ -96,6 +64,8 @@ class HumanAdvertisementParser
 
     public function parseJobLocation(string $location): ?int
     {
+        $this->initParserData();
+
         $locationParts = explode(" ", str_replace(',', '', mb_strtolower($location)));
 
         foreach (self::$locations as $locationName => $locationId) {
@@ -105,5 +75,39 @@ class HumanAdvertisementParser
         }
 
         return null;
+    }
+
+    protected function initParserData(): void
+    {
+        if ([] === self::$levels) {
+            /** @var JobLevel $item */
+            foreach (JobLevel::all() as $item) {
+                self::$levels[$item->name] = $item->keywords;
+            }
+        }
+
+        if ([] === self::$stacks) {
+            /** @var JobStack $item */
+            foreach (JobStack::all() as $item) {
+                self::$stacks[$item->name] = [
+                    'keywords' => $item->keywords,
+                    'defaultPosition' => $item->jobPosition ? $item->jobPosition->name : null,
+                ];
+            }
+        }
+
+        if ([] === self::$positions) {
+            /** @var JobPosition $item */
+            foreach (JobPosition::all() as $item) {
+                self::$positions[$item->name] = $item->keywords;
+            }
+        }
+
+        if ([] === self::$locations) {
+            /** @var Location $item */
+            foreach (Location::all() as $item) {
+                self::$locations[$item->location] = $item->id;
+            }
+        }
     }
 }
