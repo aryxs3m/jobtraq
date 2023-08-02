c99d86f619f33e41f17615b54930b0a4aba67e44
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2023-08-02 10:15:12 +0200

    fix scrape scheduling

--- laravel-backend/app/Console/Kernel.php
+++ laravel-backend/app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('jtq:crawl')->dailyAt('02:00');
+        $schedule->command('jtq:scrape')->dailyAt('02:00');
     }
 
     /**
