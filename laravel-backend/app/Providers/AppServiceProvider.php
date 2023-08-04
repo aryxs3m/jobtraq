427a8d9cfa1609545d489e68cd07abec4eb035e1
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2023-08-04 17:26:36 +0200

    fix force_https option to backend

--- laravel-backend/app/Providers/AppServiceProvider.php
+++ laravel-backend/app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (config('app.force_https', false)) {
-            URL::forceSchema("https");
+            URL::forceScheme("https");
         }
     }
 }
