530b35f1e4e255a5facb1a29d117214f3c76346f
Tóth Patrik <patrik.toth@digitalbeaver.hu>
2023-08-13 22:07:52 +0200

    admin login bg csere

--- laravel-backend/resources/views/auth/login.blade.php
+++ laravel-backend/resources/views/auth/login.blade.php
@@ -5,9 +5,6 @@
     <div class="row auth-row g-0">
         <div class="col bg-col d-flex flex-column">
             <img src="logo_Light.svg" alt="JobTraq Logo" class="p-5" style="max-width: 300px;">
-            <p class="ms-5 mb-5 mt-auto small">
-                Image by <a href="https://www.freepik.com/free-photo/fern-leaves-black-background_2518455.htm#query=dark%20plants&position=12&from_view=search&track=ais">Freepik</a>
-            </p>
         </div>
         <div class="col col-lg-4 form-col bg-dark-subtle">
             <div class="container d-flex flex-column justify-content-center px-5">
